fa1d5b7aecb168243d24b9dacc75e879e01b6495
petrushka <petrushkin@yandex.ru>
2012-09-26 00:55:38 +0400

    Ench: get rid of trash

--- parser.rb
+++ parser.rb
@@ -11,9 +11,6 @@ class Parser < Parslet::Parser
 
   rule(:vertical)      { str('|') }
 
-#  rule(:space)      { match('\s').repeat(1) }
-#  rule(:space?)     { space.maybe }
-
   rule(:sentence) { ( match("[()|]").absent? >> any).repeat(1) }
   rule(:body) {  and_sentences.as(:and_sentences)| sentence.as(:sentence)| simple_or_sentences.as(:simple_or_sentences) | or_sentences   }
 
@@ -26,26 +23,14 @@ end
 
 class Transform < Parslet::Transform
   rule(:and_sentences => subtree(:x) ) {
-    # innermost :m will contain nil
     x.join("")
   }
 
   rule(:sentence => simple(:x) ) {
-    # innermost :m will contain nil
     x
   }
 
   rule(:or_sentences => subtree(:x) ) {
-    # innermost :m will contain nil
     x[rand(x.length)]
   }
 end
-#!.each do |pexp|
-  #begin
-    #result = parser.parse(pexp)
-    #puts "#{"%20s"%pexp}: #{result.inspect} (#{transform.apply(result)} parens)"
-  #rescue Parslet::ParseFailed => m
-    #puts "#{"%20s"%pexp}: #{m}"
-  #end
-  #puts
-#end
